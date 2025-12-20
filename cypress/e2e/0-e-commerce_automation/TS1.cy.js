3ba73b6cf1cbcaaafa2c7c272bd67cecf69b8b47
Łukasz Posmyk <lukasz@MacBook-Air-ukasz.local>
2025-12-20 11:45:34 +0100

    Zaktualizowano testy TS1 dla sekcji instruktora

--- cypress/e2e/0-e-commerce_automation/TS1.cy.js
+++ cypress/e2e/0-e-commerce_automation/TS1.cy.js
@@ -5,23 +5,9 @@ describe('my first scenario', () => {
       cy.visit('https://fabrykatestow.pl');
       cy.get('a').contains('Kursy').click();
       cy.get('.elementor-button-text').eq(3).click();
-      // cy.get('h1.elementor-heading-title').contains('Kto nauczy Cię testów automatycznych?').scrollIntoView().should('be.visible').screenshot();
       
-
-      cy.get('h1.elementor-heading-title')
-  .contains('Kto nauczy Cię testów automatycznych?')
-  .scrollIntoView();
-
-cy.wait(4000); // 0.5 sekundy zazwyczaj wystarczy, by animacja dobiegła końca
-
-// Łapiemy główny kontener sekcji "Kto nauczy Cię testów..."
-cy.get('.elementor-element-4ee1fd56') // To jest ID widgetu z nagłówkiem
-  .parent() // Wychodzimy do wspólnego rodzica całej sekcji
-  .screenshot('profil-instruktora');
-
-// cy.get('h1.elementor-heading-title')
-//   .contains('Kto nauczy Cię testów automatycznych?')
-//   .screenshot('naglowek-stabilny');
-      
+      cy.get('h1.elementor-heading-title').contains('Kto nauczy Cię testów automatycznych?').scrollIntoView();
+      cy.wait(4000);
+      cy.get('.elementor-element-4ee1fd56') .parent() .screenshot('profil-instruktora');
    });
 });
