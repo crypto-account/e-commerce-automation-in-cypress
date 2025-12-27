44adf977bf66ac5678eb4dde703a5bc42e72a34a
Łukasz Posmyk <lukasz@MacBook-Air-ukasz.local>
2025-12-27 17:53:42 +0100

    feature: remote dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'fqyazx',
   e2e: {
     baseUrl: 'https://tapsshop.pl/',
 
--- test1.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello, World!");
-console.log("This is a test file.");
-console.log("Testing JavaScript code execution.");
-console.log("All tests passed successfully.");
-console.log("End of test file.");
